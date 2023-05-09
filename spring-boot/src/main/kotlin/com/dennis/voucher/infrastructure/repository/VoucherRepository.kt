c8d93002fcaa4a9bfe6a72898bff27daf2df41f1
Dennis Shi-Rudo <42831102+shi-rudo@users.noreply.github.com>
2023-05-09 12:00:56 +0200

    Added note about PESSIMISTIC_WRITE

--- spring-boot/src/main/kotlin/com/dennis/voucher/infrastructure/repository/VoucherRepository.kt
+++ spring-boot/src/main/kotlin/com/dennis/voucher/infrastructure/repository/VoucherRepository.kt
@@ -10,11 +10,13 @@ import java.util.*
 // TODO: IMPLEMENT ASYNC OR REACTIVE REPOSITORIES
 interface VoucherRepository : CrudRepository<Voucher, UUID> {
 
-    // ensures that no other transaction can update the Voucher entity until the current transaction completes
+    // PESSIMISTIC_WRITE: You get a lock on the record at the beginning of the transaction to write it.
+    // You are saying "I am going to update this record, so no one can read or write until I am done". This means that
+    // both attempts to perform a PESSIMISTIC_READ or PESSIMISTIC_WRITE will fail.
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findFirstByStatusIs(status: String): Voucher?
 
     fun findByEmail(email: String): Voucher?
-
 
 }
